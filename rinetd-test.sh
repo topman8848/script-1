811905506850ed01de4b044071b507b687fc4797
Mcl <mixool0204@gmail.com>
2017-09-16 00:49:02 +0800

    Create rinetd-test.sh

---
+++ rinetd-test.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+# Usage:
+#   curl https://raw.githubusercontent.com/mixool/script/master/rinetd-test.sh | bash
+
+export green='\033[0;32m'
+export plain='\033[0m'
+
+export URL="https://raw.githubusercontent.com/mixool/script/source/rinetd_bbr_powered"
+export NAME="rinetd"
+
+if [ "$(id -u)" != "0" ]; then
+    echo "ERROR: Please run as root"
+    exit 1
+fi
+
+for CMD in curl iptables grep cut xargs systemctl ip awk
+do
+	if ! type -p ${CMD}; then
+		echo -e "\e[1;31mtool ${CMD} is not installed, abort.\e[0m"
+		exit 1
+	fi
+done
+
+echo "Download $NAME from $URL"
+curl -L "${URL}" >/root/$NAME
+chmod +x /root/$NAME
+
+echo "Generate /root/$NAME.conf"
+read -p "Input ports you want to speed up:" LIST </dev/tty
+for d in $LIST
+do          
+cat <<EOF > /root/$NAME.conf
+0.0.0.0 $d 0.0.0.0 $d 
+EOF
+done 
+
+IFACE=$(ip -4 addr | awk '{if ($1 ~ /inet/ && $NF ~ /^[ve]/) {a=$NF}} END{print a}')
+
+echo "Generate /etc/systemd/system/$NAME.service"
+
+cat <<EOF > /etc/systemd/system/$NAME.service
+[Unit]
+Description=$NAME
+
+[Service]
+ExecStart=/root/$NAME -f -c /root/$NAME.conf raw ${IFACE}
+Restart=always
+User=root
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+echo "4. Enable $NAME Service"
+systemctl enable $NAME.service
+
+echo "5. Start $NAME Service"
+systemctl start $NAME.service
+
+if systemctl status $NAME >/dev/null; then
+	echo "$NAME started."
+	echo "By default, it only speed up port 80 and 443."
+	echo -e "${green}vi /etc/systemd/system/$NAME.service${plain} as needed."
+	echo -e "${green}killall -9 $NAME${plain} for restart."
+else
+	echo "$NAME start failed."
+fi
