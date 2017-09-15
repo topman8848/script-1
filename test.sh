3d78f873a7048c4650e570396a47c9204e13821b
Mcl <mixool0204@gmail.com>
2017-09-15 22:30:07 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Usage:
-#   wget --no-check-certificate https://raw.githubusercontent.com/mixool/script/master/test.sh
-#   chmod +x test.sh
-#   ./test.sh
+#   wget --no-check-certificate https://raw.githubusercontent.com/mixool/script/master/easystart.sh
+#   chmod +x easystart.sh
+#   ./easystart.sh
 
 
 if [ "$(id -u)" != "0" ]; then
@@ -13,27 +13,26 @@ fi
 export green='\033[0;32m'
 export plain='\033[0m'
 
-read_p=$(test -n "$BASH_VERSION" && echo 'read -ep' || echo 'read -p')
-$read_p "Download url: " URL </dev/tty
-$read_p "Service name: " NAME </dev/tty
-$read_p "Start command: " DO </dev/tty
+read -p "Download url: " URL </dev/tty
+read -p "Service name: " NAME </dev/tty
+read -p "Start command: " DO </dev/tty
 
-echo -e "${green}Clean up ${NAME}${plain}"
-systemctl disable ${NAME}.service
-killall -9 ${NAME}
-rm -rf /root/${NAME} /etc/systemd/system/${NAME}.service
+echo -e "${green}Clean up $NAME${plain}"
+systemctl disable $NAME.service
+killall -9 $NAME
+rm -rf /root/$NAME /etc/systemd/system/$NAME.service
 
-echo "Download ${NAME} from ${URL}"
-wget --no-check-certificate ${URL} -O /root/${NAME}
-chmod +x /root/${NAME}
+echo "Download $NAME from $URL"
+wget --no-check-certificate $URL -O /root/$NAME
+chmod +x /root/$NAME
 
-echo "Generate /etc/systemd/system/${NAME}.service"
-cat <<EOF > /etc/systemd/system/${NAME}.service
+echo "Generate /etc/systemd/system/$NAME.service"
+cat <<EOF > /etc/systemd/system/$NAME.service
 [Unit]
-Description=${NAME}
+Description=$NAME
 
 [Service]
-ExecStart=/root/${NAME} ${DO}
+ExecStart=/root/$NAME $DO
 Restart=always
 User=root
 
@@ -41,16 +40,16 @@ User=root
 WantedBy=multi-user.target
 EOF
 
-echo "Enable ${NAME} Service"
-systemctl enable ${NAME}.service
+echo "Enable $NAME Service"
+systemctl enable $NAME.service
 
-echo "Start ${NAME} Service"
-systemctl start ${NAME}.service
+echo "Start $NAME Service"
+systemctl start $NAME.service
 
-if systemctl status ${NAME} >/dev/null; then
-	echo "${NAME} started."
-	echo -e "${green}vi /etc/systemd/system/${NAME}.service${plain} or run this script again as needed."
-	echo -e "${green}killall -9 ${NAME}${plain} for restart."
+if systemctl status $NAME >/dev/null; then
+	echo "$NAME started."
+	echo -e "${green}vi /etc/systemd/system/$NAME.service${plain} or run this script again as needed."
+	echo -e "${green}killall -9 $NAME${plain} for restart."
 else
-	echo "${NAME} start failed."
+	echo "$NAME start failed."
 fi
