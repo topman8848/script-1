9c3a956f3632af84535c2a72ffbb3ac99738d69a
Mcl <mixool0204@gmail.com>
2017-09-16 01:21:08 +0800

    Update rinetd-test.sh

--- rinetd-test.sh
+++ rinetd-test.sh
@@ -64,7 +64,7 @@ systemctl start $NAME.service
 
 if systemctl status $NAME >/dev/null; then
 	echo "$NAME started."
-	echo "Speed up port ${green}$LIST${plain}"
+	echo -e "Speed up port ${green}$LIST${plain}"
 	echo -e "${green} vi /root/$NAME.conf${plain} as needed."
 	echo -e "${green}killall -9 $NAME${plain} for restart."
 else
