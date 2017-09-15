7dcc4d8b79582425f25f95a24e611bd1f42ef2ad
Mcl <mixool0204@gmail.com>
2017-09-15 14:22:12 +0800

    Update nibbleblog-v4.0.2-markdown.sh

--- nibbleblog-v4.0.2-markdown.sh
+++ nibbleblog-v4.0.2-markdown.sh
@@ -3,9 +3,7 @@
 #   curl https://raw.githubusercontent.com/mixool/script/master/nibbleblog-v4.0.2-markdown.sh | bash
 
 export URL="https://raw.githubusercontent.com/mixool/script/source/nibbleblog-v4.0.2-markdown.zip"
-export red='\033[0;31m'
 export green='\033[0;32m'
-export yellow='\033[0;33m'
 export plain='\033[0m'
 
 if [ "$(id -u)" != "0" ]; then
