309b5ac9691c1c4ef53b5daa4f7cf4851621ee27
Mcl <mixool0204@gmail.com>
2017-09-15 14:01:39 +0800

    Update nibbleblog-v4.0.2-markdown.sh

--- nibbleblog-v4.0.2-markdown.sh
+++ nibbleblog-v4.0.2-markdown.sh
@@ -2,20 +2,20 @@
 # Usage:
 #   curl https://raw.githubusercontent.com/mixool/script/master/nibbleblog-v4.0.2-markdown.sh | bash
 
-export URL="http://sourceforge.net/projects/nibbleblog/files/v4.0/nibbleblog-v4.0.2-markdown.zip"
+export URL="https://raw.githubusercontent.com/mixool/script/source/nibbleblog-v4.0.2-markdown.zip"
 
 if [ "$(id -u)" != "0" ]; then
     echo "ERROR: Please run as root"
     exit 1
 fi
 
-echo "1. Install updating"
+echo "Install updating"
 apt-get update
 apt-get install apache2 unzip php5-common libapache2-mod-php5  php5-gd php5-mcrypt -y
 service apache2 restart
 clear
 
-echo "2. Download nibbleblog-markdown from $RINET_URL and setup"
+echo "Download nibbleblog-markdown from $RINET_URL and setup"
 rm -rf /var/www/html
 cd /var/www
 wget --no-check-certificate "${URL}"
@@ -23,6 +23,6 @@ unzip nibbleblog-v4.0.2-markdown.zip
 rm -rf nibbleblog-v4.0.2-markdown.zip
 mv nibbleblog-markdown html
 chmod 777 /var/www/html/content
+clear
 
-
-echo "3. Congratulations, nibbleblog-markdown install completed!"
+echo "Congratulations, nibbleblog-markdown install completed!"
