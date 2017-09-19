03893156f16627948354718a6f360958b0497da2
Mcl <mixool0204@gmail.com>
2017-09-19 22:57:42 +0800

    Update rinetd.sh

--- rinetd.sh
+++ rinetd.sh
@@ -41,7 +41,8 @@ cat <<EOF >> /root/$NAME.conf
 EOF
 done 
 
-for b in $( $(seq $PORT{[0]} $PORT{[1]}) $(seq $PORT{[1]} $PORT{[0]}) )
+# TWO=( $(seq ${PORT[0]} ${PORT[1]}) $(seq ${PORT[1]} ${PORT[0]}) )
+for b in $( $( $(seq ${PORT[0]} ${PORT[1]}) $(seq ${PORT[1]} ${PORT[0]}) )
 do          
 cat <<EOF >> /root/$NAME.conf
 0.0.0.0 $b 0.0.0.0 $b
