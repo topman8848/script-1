9a235bf643c60f6ffc1823077363986da5cf7df7
Mcl <mixool0204@gmail.com>
2017-10-09 15:45:56 +0800

    Update rinetd.sh

--- rinetd.sh
+++ rinetd.sh
@@ -83,7 +83,7 @@ systemctl start $NAME.service
 if systemctl status $NAME >/dev/null; then
 	echo "$NAME started."
 	echo -e "${green}vi /root/$NAME.conf${plain} as needed."
-	echo -e "${green}systemctl restart $NAME.service${plain} for restart."
+	echo -e "${green}systemctl daemon-reload && systemctl restart $NAME.service${plain} for restart."
 else
 	echo "$NAME start failed."
 fi
