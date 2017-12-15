93eac222b4ca938b4325b10f9fd55fdd051f73b5
Mcl <mixool0204@gmail.com>
2017-12-15 18:03:28 +0800

    Update debian-xmr.sh

--- debian-xmr.sh
+++ debian-xmr.sh
@@ -2,14 +2,16 @@
 # Usage:
 #   wget --no-check-certificate  https://raw.githubusercontent.com/mixool/script/master/debian-xmr.sh && chmod +x debian-xmr.sh &&./debian-xmr.sh
 
+add-apt-repository ppa:ubuntu-toolchain-r/test
 apt update
-apt install libmicrohttpd-dev libssl-dev build-essential libhwloc-dev git curl -y
+apt install -y gcc-5 g++-5 make curl git cpulimit
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 1 --slave /usr/bin/g++ g++ /usr/bin/g++-5
 curl -L http://www.cmake.org/files/v3.4/cmake-3.4.1.tar.gz | tar -xvzf - -C /tmp/
 cd /tmp/cmake-3.4.1/ && ./configure && make && sudo make install && cd -
 update-alternatives --install /usr/bin/cmake cmake /usr/local/bin/cmake 1 --force
-git clone https://github.com/fireice-uk/xmr-stak.git
-mkdir xmr-stak/build
-cd xmr-stak/build
+apt install -y libmicrohttpd-dev libssl-dev libhwloc-dev
+git clone https://github.com/fireice-uk/xmr-stak-cpu xmr-stak
+cd xmr-stak
 cmake .. -DCMAKE_INSTALL_PREFIX=$HOME/xmr-stak-cpu
 make install
 
