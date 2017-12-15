6ec7b6e94f7fb57119af812bee770a8a6294cd10
Mcl <mixool0204@gmail.com>
2017-12-15 19:24:30 +0800

    Update xmr-cpu.sh

--- xmr-cpu.sh
+++ xmr-cpu.sh
@@ -2,9 +2,10 @@
 # Usage:
 #   wget --no-check-certificate  https://raw.githubusercontent.com/mixool/script/master/xmr-cpu.sh && chmod +x xmr-cpu.sh && echo | ./xmr-cpu.sh
 
+#Compile xmr-stak for Ubuntu 14.04
 apt-get update
 apt-get install -y python-software-properties
-echo | add-apt-repository ppa:ubuntu-toolchain-r/test
+add-apt-repository ppa:ubuntu-toolchain-r/test
 apt update
 apt install -y gcc-5 g++-5 make curl git cpulimit
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 1 --slave /usr/bin/g++ g++ /usr/bin/g++-5
@@ -12,7 +13,8 @@ curl -L http://www.cmake.org/files/v3.4/cmake-3.4.1.tar.gz | tar -xvzf - -C /tmp
 cd /tmp/cmake-3.4.1/ && ./configure && make && sudo make install && cd -
 update-alternatives --install /usr/bin/cmake cmake /usr/local/bin/cmake 1 --force
 apt install -y libmicrohttpd-dev libssl-dev libhwloc-dev
-git clone https://github.com/fireice-uk/xmr-stak-cpu xmr-stak
-cd xmr-stak
-cmake .. -DCMAKE_INSTALL_PREFIX=$HOME/xmr-stak-cpu
+git clone https://github.com/fireice-uk/xmr-stak.git
+mkdir xmr-stak/build
+cd xmr-stak/build
+cmake ..
 make install
