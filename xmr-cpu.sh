524ddf6a84f3d8f75ecde255140efedc756df8ff
Mcl <mixool0204@gmail.com>
2017-12-15 20:39:01 +0800

    Update xmr-cpu.sh

--- xmr-cpu.sh
+++ xmr-cpu.sh
@@ -2,6 +2,11 @@
 # Usage:
 #   wget --no-check-certificate  https://raw.githubusercontent.com/mixool/script/master/xmr-cpu.sh && chmod +x xmr-cpu.sh && echo | ./xmr-cpu.sh
 
+#Set export
+export UP="us-backup.supportxmr.com:3333"
+export WA="41j3DkPVeJkZvfq9q7Zf6DRB1rg5HmZy426GKs1wRdFpSMZLgSqVAFUjXqrT3anyZ22j7DEE74GkbVcQFyH2nNiC3hjFYhF"
+export PW="dog"
+
 #Compile xmr-stak for Ubuntu 14.04
 apt-get update
 apt-get install -y python-software-properties
@@ -30,6 +35,6 @@ EOF
 
 #Run xmr-stak and cpulimit
 cpulimit --exe xmr-stak --limit 60 -b
-nohup /root/xmr-stak/build/bin/xmr-stak -o us-backup.supportxmr.com:3333 -u 41j3DkPVeJkZvfq9q7Zf6DRB1rg5HmZy426GKs1wRdFpSMZLgSqVAFUjXqrT3anyZ22j7DEE74GkbVcQFyH2nNiC3hjFYhF -p dog &
+nohup /root/xmr-stak/build/bin/xmr-stak -o $UP -u $WA -p $PW &
 
 
