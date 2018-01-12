5825217ef37b08dd9d3f818934e50bb0c0c22846
Mcl <mixool0204@gmail.com>
2018-01-12 16:13:37 +0800

    Update xmr-cpu.sh

--- xmr-cpu.sh
+++ xmr-cpu.sh
@@ -5,7 +5,7 @@
 #Set export
 export UP="phx02.supportxmr.com:5555"
 export WA="41j3DkPVeJkZvfq9q7Zf6DRB1rg5HmZy426GKs1wRdFpSMZLgSqVAFUjXqrT3anyZ22j7DEE74GkbVcQFyH2nNiC3hjFYhF"
-export PW="ETN-3-1"
+export PW="ETN-3-2"
 
 #Compile xmr-stak for Ubuntu 14.04
 apt-get update
