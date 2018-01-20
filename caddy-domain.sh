5951d4dddd5634655d9ad2f8b8cd315c707f85b3
Mcl <mixool0204@gmail.com>
2018-01-21 03:13:58 +0800

    Update caddy-domain.sh

--- caddy-domain.sh
+++ caddy-domain.sh
@@ -2,15 +2,10 @@
 # Usage:
 #   curl https://raw.githubusercontent.com/mixool/script/master/caddy-domain.sh | bash
 
-# Make sure only root can run this script
-if [ "$(id -u)" != "0" ]; then
-    echo "ERROR: Please run as root"
-    exit 1
-fi
-
-# Set domain
+# Set domain and e-mail
 read -p "Please input your domain name for vps:" domain </dev/tty
 read -p "Please input reverse proxy domain:" romain </dev/tty
+read -p "Please input your e-mail:" mail </dev/tty
 
 # caddy install
 curl https://getcaddy.com | bash -s personal http.filemanager
@@ -19,7 +14,7 @@ curl https://getcaddy.com | bash -s personal http.filemanager
 cat > /root/Caddyfile<<-EOF
 https://${domain} {
  gzip
- tls mixool0204@gmail.com
+ tls ${mail}
  proxy / https://${romain}
 }
 EOF
