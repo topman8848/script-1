80cb510c619753b8d81eaed1eb23d15885c002b4
Mcl <mixool0204@gmail.com>
2018-02-04 04:19:09 +0800

    Update myself.sh

--- myself.sh
+++ myself.sh
@@ -23,22 +23,25 @@ mkdir /root/hostloc
 mkdir /root/BilibiliHelper
 
 #hostloc
+wget https://raw.githubusercontent.com/mixool/script/master/hostloc.py -O /root/hostloc/hostloc1.py
+wget https://raw.githubusercontent.com/mixool/script/master/hostloc.py -O /root/hostloc/hostloc2.py
+wget https://raw.githubusercontent.com/mixool/script/master/hostloc.py -O /root/hostloc/hostloc3.py
+wget https://raw.githubusercontent.com/mixool/script/master/hostloc.py -O /root/hostloc/hostloc4.py
+wget https://raw.githubusercontent.com/mixool/script/master/hostloc.py -O /root/hostloc/hostloc5.py
 wget https://raw.githubusercontent.com/mixool/script/master/hostloc.py -O /root/hostloc/hostloc.py
-wget https://raw.githubusercontent.com/mixool/script/master/hostloc.py -O /root/hostloc/hostmoc.py
-wget https://raw.githubusercontent.com/mixool/script/master/hostloc.py -O /root/hostloc/hostxoc.py
 
 #bilibiliHelper
 wget https://github.com/mixool/script/raw/source/Bilibili.php -O /root/BilibiliHelper/Bilibili.php
 wget https://github.com/mixool/script/raw/source/Bilibilirun.php -O /root/BilibiliHelper/index.php
 
 #/etc/crontab
-echo "1 0 * * * root python /root/hostloc/hostloc.py" >> /etc/crontab
-echo "2 0 * * * root python /root/hostloc/hostmoc.py" >> /etc/crontab
-echo "4 0 * * * root python /root/hostloc/hostxoc.py" >> /etc/crontab
+echo "1 0 * * * root python /root/hostloc/hostloc1.py" >> /etc/crontab
+echo "2 0 * * * root python /root/hostloc/hostloc2.py" >> /etc/crontab
+echo "3 0 * * * root python /root/hostloc/hostloc3.py" >> /etc/crontab
+echo "4 0 * * * root python /root/hostloc/hostloc4.py" >> /etc/crontab
+echo "5 0 * * * root python /root/hostloc/hostloc5.py" >> /etc/crontab
 echo "#" >> /etc/crontab
 echo "3 0 * * * root php /root/BilibiliHelper/index.php > /root/BilibiliHelper/log.txt" >> /etc/crontab
-echo "#" >> /etc/crontab
-echo -e "30 */4 * * * root curl -X POST \"https://openapi.daocloud.io/v1/apps/ae20ae7b-5ddd-42d7-b7d4-e761533e9e40/actions/redeploy\" -H \"Authorization: token 3od80jtlyaaiw4s024s77oprj7ttcukq6eqh5wp4\" -H \"Content-Type: application/json\" -d '{\"release_name\": \"latest\"}'" >> /etc/crontab
 echo "#" >> /etc/crontab
 
 #Informations
