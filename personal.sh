a84af17126250baef599fd9a1317054926f6aeb7
Mcl <mixool0204@gmail.com>
2018-02-27 13:58:52 +0800

    Update personal.sh

--- personal.sh
+++ personal.sh
@@ -5,3 +5,4 @@
 read -p "Input url: " MYURL </dev/tty
 
 curl -k  $MYURL | grep -oE "ssr?\://.*" | awk '{print $1}' | awk -F\" '{print $1}' | awk -F\< '{print $1}' | awk -F\\ '{print $1}' | sort -u >thanks.ss
+cat thanks.ss
