25b3ea9b772c9b9414bec360802ea3fd9e47a05d
Mcl <mixool0204@gmail.com>
2018-02-27 13:38:03 +0800

    Update personal.sh

--- personal.sh
+++ personal.sh
@@ -4,16 +4,4 @@
 
 read -p "Input url: " MYURL </dev/tty
 
-curl -k  $MYURL -o 0.conf
-
-grep -oE "ssr?\://.*" 0.conf >1.conf
-
-awk '{print $1}' 1.conf >2.conf
-
-awk -F\" '{print $1}' 2.conf >3.conf
-
-awk -F\< '{print $1}' 3.conf  >4.conf
-
-awk -F\\ '{print $1}' 4.conf  >ok.conf
-
-sort -u ok.conf | base64 >index.html
+curl -k  $MYURL | grep -oE "ssr?\://.*" | awk '{print $1}' | awk -F\" '{print $1}' | awk -F\< '{print $1}' | awk -F\\ '{print $1}' | sort -u | base64 >index.html
