090d58f044327487886193ebe5334d1ea309d7ec
Mcl <mixool0204@gmail.com>
2018-03-02 12:36:42 +0800

    Update personal.sh

--- personal.sh
+++ personal.sh
@@ -3,5 +3,9 @@
 #   curl https://raw.githubusercontent.com/mixool/script/master/personal.sh | bash
 
 read -p "Input url: " URL </dev/tty
+read -p "Save as: " SA </dev/tty
 
-curl -k  $URL | grep -oE "ssr?://[^x][a-zA-Z0-9]+" | sort -u
+curl -k  $URL | grep -oE "ssr?://[^x][a-zA-Z0-9]+" | sort -u >$SA
+
+#Crontab
+(crontab -l ; echo -e "0 * * * * curl -k  $URL | grep -oE "ssr?://[^x][a-zA-Z0-9]+" | sort -u >$SA") | crontab -
