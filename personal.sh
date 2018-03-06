8700b884e944f2fc11939197554890e24ea38057
Mcl <mixool0204@gmail.com>
2018-03-06 20:27:08 +0800

    Update personal.sh

--- personal.sh
+++ personal.sh
@@ -7,6 +7,7 @@ read -p "Input url: " URL </dev/tty
 
 curl -k  $URL | grep -oE "ssr?://\w{18,}" | sort -u
 #curl -k  $URL | grep -oE "ssr?://\w{18,}" | sort -u >>$SA
+#grep -oE "ssr?://\w{18,}" | awk -F"Jmdyb3VwP|mZ3JvdXA9|cm91cD" '{print $1}' | sort -u
 
 # Crontab
 #(crontab -l ; echo -e "0 * * * * curl -k  $URL | grep -oE "ssr?://\w{18,}" | sort -u >>$SA") | crontab -
