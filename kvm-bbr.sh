2ed0e0050744856b9acd427b3ae4d48d1989d9ae
Mcl <mixool0204@gmail.com>
2018-05-01 12:53:21 +0800

    Update kvm-bbr.sh

--- kvm-bbr.sh
+++ kvm-bbr.sh
@@ -11,6 +11,7 @@ fi
 #Install bbr 
 wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.16/linux-image-4.16.0-041600-generic_4.16.0-041600.201804012230_amd64.deb
 dpkg -i linux-image-4.16.0-041600-generic_4.16.0-041600.201804012230_amd64.deb
+rm linux-image-4.16.0-041600-generic_4.16.0-041600.201804012230_amd64.deb
 
 echo "net.core.default_qdisc = fq" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control = bbr" >> /etc/sysctl.conf
