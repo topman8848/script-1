e35b9b5eabd938bf533a60f49a4e392a330871cc
Mcl <mixool0204@gmail.com>
2019-02-22 21:51:34 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -7,7 +7,7 @@ do
 	echo Downloading $i...
 	Length=$(wget -SO- $Url 2>&1 >/dev/null | grep -oE "Content-Length: [0-9]+" | grep -oE "[0-9]+")
 	Total=$(awk 'BEGIN{printf ('$i'*'$Length')}')
-	Ltotal=$(awk 'BEGIN{printf "%.f\n",('$MBlimit')*1024*1024-'$Length'}')
+	Ltotal=$(awk 'BEGIN{printf "%.f\n",('$MBlimit'*1024*1024-'$Length')}')
 	MBtotal=$(awk 'BEGIN{printf "%.4f\n",('$i'*'$Length'/1024/1024)}')
 	GBtotal=$(awk 'BEGIN{printf "%.4f\n",('$i'*'$Length'/1024/1024/1024)}')
 	
