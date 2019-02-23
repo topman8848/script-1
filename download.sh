5846ca160a260afba460e0a9ae9dcf27b02437f4
Mcl <mixool0204@gmail.com>
2019-02-23 11:15:48 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -8,8 +8,8 @@ MBlimit=1024
 ##############################
 
 MBlimit=$(awk 'BEGIN{printf "%.f\n",('$MBlimit'*1024*1024)}')
-Length=$(curl -I $Url | grep -oE "Content-Length: [0-9]+" | grep -oE "[0-9]+")
-length=$(curl -I $url | grep -oE "Content-Length: [0-9]+" | grep -oE "[0-9]+")
+Length=$(curl -s -I $Url | grep -oE "Content-Length: [0-9]+" | grep -oE "[0-9]+")
+length=$(curl -s -I $url | grep -oE "Content-Length: [0-9]+" | grep -oE "[0-9]+")
 T=$(awk 'BEGIN{printf int('$MBlimit'/'$Length')}')
 t=$(awk 'BEGIN{printf int('$MBlimit'%'$Length'/'$length')}')
 FMB=$(awk 'BEGIN{printf "%.4f\n",(('$MBlimit'-('$MBlimit'%'$Length'%'$length'))/1024/1024)}')
@@ -18,7 +18,7 @@ FGB=$(awk 'BEGIN{printf "%.4f\n",(('$MBlimit'-('$MBlimit'%'$Length'%'$length'))/
 for((i = 1; i <= T; i++))
 do
 	echo Downloading $i...
-	wget -O /dev/null $Url
+	curl -o /dev/null $Url
 	Total=$(awk 'BEGIN{printf ('$i'*'$Length')}')
 	MBtotal=$(awk 'BEGIN{printf "%.4f\n",('$i'*'$Length'/1024/1024)}')
 	GBtotal=$(awk 'BEGIN{printf "%.4f\n",('$i'*'$Length'/1024/1024/1024)}')
@@ -28,7 +28,7 @@ done
 for((j = 1; j <= t; j++))
 do
 	echo Still downloading...
-	wget -q -O /dev/null $url
+	curl -s -o /dev/null $url
 done
 
 echo $FMB MB had been downloaded, It is about $FGB GB. Thanks!
