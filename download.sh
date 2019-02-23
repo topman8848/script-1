9611db0b2eaf9ae46d8f9bdaccf72c98405bd0b7
Mcl <mixool0204@gmail.com>
2019-02-23 10:32:44 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,21 +1,34 @@
-#!binbash
+#!/bin/bash
+# Usage:
+#   curl https://raw.githubusercontent.com/mixool/script/master/download.sh | bash
+##############################
 Url=http://download.alicdn.com/wireless/taobao4android/latest/702757.apk
+url=http://cesu.cqwin.com/ddb_update/clientdownload/DTestClientSetupCQ.zip
 MBlimit=1024
+##############################
 
-for((i = 1; i >= 1; i++))
+MBlimit=$(awk 'BEGIN{printf "%.f\n",('$MBlimit'*1024*1024)}')
+Length=$(curl -I $Url | grep -oE "Content-Length: [0-9]+" | grep -oE "[0-9]+")
+length=$(curl -I $url | grep -oE "Content-Length: [0-9]+" | grep -oE "[0-9]+")
+T=$(awk 'BEGIN{printf int('$MBlimit'/'$Length')}')
+t=$(awk 'BEGIN{printf int('$MBlimit'%'$Length'/'$length')}')
+FMB=$(awk 'BEGIN{printf "%.4f\n",(('$MBlimit'-('$MBlimit'%'$Length'%'$length'))/1024/1024)}')
+FGB=$(awk 'BEGIN{printf "%.4f\n",(('$MBlimit'-('$MBlimit'%'$Length'%'$length'))/1024/1024/1024)}')
+
+for((i = 1; i <= T; i++))
 do
 	echo Downloading $i...
-	Length=$(wget -SO- $Url 2>&1 >/dev/null | grep -oE "Content-Length: [0-9]+" | grep -oE "[0-9]+")
+	wget -O /dev/null $Url
 	Total=$(awk 'BEGIN{printf ('$i'*'$Length')}')
-	Ltotal=$(awk 'BEGIN{printf "%.f\n",('$MBlimit'*1024*1024-'$Length')}')
 	MBtotal=$(awk 'BEGIN{printf "%.4f\n",('$i'*'$Length'/1024/1024)}')
 	GBtotal=$(awk 'BEGIN{printf "%.4f\n",('$i'*'$Length'/1024/1024/1024)}')
-	
-	if [ "$Ltotal" -lt "$Total" ]
-	then
-	echo $MBtotal MB had been downloaded, That is about $GBtotal GB, Thanks!
-	break
-	fi
 	echo $MBtotal MB had been downloaded, That is about $GBtotal GB.
-	
 done
+
+for((j = 1; j <= t; j++))
+do
+	echo Still downloading...
+	wget -O /dev/null $url
+done
+
+echo $FMB MB had been downloaded, It is about $FGB GB. Thanks!
