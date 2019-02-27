93e68b8da2135a49d6186b5835a9012fe57a60a7
Mcl <mixool0204@gmail.com>
2019-02-27 18:46:29 +0800

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -2,7 +2,7 @@
 # Usage:
 #   curl https://raw.githubusercontent.com/mixool/script/master/curl.sh | bash
 ###Total download depends on MBlimit,speed depends on Url,precision depends on url，change them if necessary.
-MBlimit=1024
+MBlimit=300
 Url=http://download.alicdn.com/wireless/taobao4android/latest/702757.apk
 url=http://cesu.cqwin.com/ddb_update/clientdownload/DTestClientSetupCQ.zip
 #############################################################################################################
@@ -10,43 +10,32 @@ url=http://cesu.cqwin.com/ddb_update/clientdownload/DTestClientSetupCQ.zip
 MBlimit=$(awk 'BEGIN{printf "%.f\n",('$MBlimit'*1024*1024)}')
 Length=$(curl -s -I $Url | grep -oE "Content-Length: [0-9]+" | grep -oE "[0-9]+")
 length=$(curl -s -I $url | grep -oE "Content-Length: [0-9]+" | grep -oE "[0-9]+")
-T=$(awk 'BEGIN{printf int('$MBlimit'/'$Length')}')
 t=$(awk 'BEGIN{printf int('$MBlimit'%'$Length'/'$length')}')
-FMB=$(awk 'BEGIN{printf "%.4f\n",(('$MBlimit'-('$MBlimit'%'$Length'%'$length'))/1024/1024)}')
-FGB=$(awk 'BEGIN{printf "%.4f\n",(('$MBlimit'-('$MBlimit'%'$Length'%'$length'))/1024/1024/1024)}')
+T=$(awk 'BEGIN{printf int('$MBlimit'/'$Length')}')
+FMB=$(awk 'BEGIN{printf "%.2f\n",(('$MBlimit'-('$MBlimit'%'$Length'%'$length'))/1024/1024)}')
+FGB=$(awk 'BEGIN{printf "%.3f\n",(('$MBlimit'-('$MBlimit'%'$Length'%'$length'))/1024/1024/1024)}')
+
+if [ "$t" -gt 0 ]; then
+	echo $(date) $(echo $url | awk -F"/" '{print $NF}') - $(awk 'BEGIN{printf "%.1f\n",('$length'/1024/1024)}') MB will be downloaded $t times...
+	for((i = 1; i <= t; i++))
+	do
+		echo -n $i.
+		curl -s -o /dev/null $url
+	done
+	echo
+	echo $(date) - $t - $(awk 'BEGIN{printf "%.1f\n",('$t'*'$length'/1024/1024)}') MB.
+	echo
+fi
 
 if [ "$T" -gt 0 ]; then
-	echo `date` Start downloading $Url ...
+	echo $(date) $(echo $Url | awk -F"/" '{print $NF}') - $(awk 'BEGIN{printf "%.1f\n",('$Length'/1024/1024)}') MB will be downloaded $T times...
+	for((j = 1; j <= T; j++))
+	do
+		echo
+		curl -o /dev/null $Url
+		echo $(date) - $j - $(awk 'BEGIN{printf "%.1f\n",('$j'*'$Length'/1024/1024)}') MB.
+		echo
+	done
 fi
 
-for((i = 1; i <= T; i++))
-do
-	echo 
-	curl -o /dev/null $Url
-	Total=$(awk 'BEGIN{printf ('$i'*'$Length')}')
-	MBtotal=$(awk 'BEGIN{printf "%.4f\n",('$i'*'$Length'/1024/1024)}')
-	GBtotal=$(awk 'BEGIN{printf "%.4f\n",('$i'*'$Length'/1024/1024/1024)}')
-	echo `date` $MBtotal MB \($GBtotal GB\) had been downloaded. Accomplished $i.
-	echo
-done
-
-if [ "$t" -gt 0 ]; then
-echo -n `date` Start downloading $url ...
-fi
-
-for((j = 1; j <= t; j++))
-do
-	echo -n .
-	curl -s -o /dev/null $url
-done
-
-if [ "$t" -gt 0 ]; then
-total=$(awk 'BEGIN{printf ('$t'*'$length')}')
-mBtotal=$(awk 'BEGIN{printf "%.4f\n",('$t'*'$length'/1024/1024)}')
-gBtotal=$(awk 'BEGIN{printf "%.4f\n",('$t'*'$length'/1024/1024/1024)}')
-echo
-echo `date` $mBtotal MB \($gBtotal GB\) had been downloaded. Accomplished $t.
-fi
-
-echo
-echo `date` Accomplished, $FMB MB \($FGB GB\) had been downloaded. Thanks!
+echo $(date) Mission $(awk 'BEGIN{printf "%.f\n",('$MBlimit'/1024/1024)}') MB. Accomplished $FMB MB \($FGB GB\) . Thanks! 
