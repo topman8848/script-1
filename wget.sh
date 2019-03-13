4925600b07e38bf0f0333e9a07448480dc9f34e6
Mcl <mixool0204@gmail.com>
2019-03-14 01:08:37 +0800

    Update wget.sh

--- wget.sh
+++ wget.sh
@@ -29,35 +29,39 @@ fi
 
 #Set China Mobile url Url by parameter $2: bash wget.sh 2048 yd
 if [ "$2" = "yd" ]; then
-url=https://wap.cmread.com/r/p/pg/212/CMREADBC_Android/CMREADBC_Android.apk
+url=http://wap.cmread.com/r/p/pg/212/CMREADBC_Android/CMREADBC_Android.apk
 Url=http://pc.miguvideo.com/MiguApi/download/MiguVideo3.3.0.105.exe
 fi
 
-length=$(wget --spider -U "$UA" $url -SO- /dev/null 2>&1 | grep -oE "Content-Length: [0-9]+" | grep -oE "[0-9]+")
-Length=$(wget --spider -U "$UA" $Url -SO- /dev/null 2>&1 | grep -oE "Content-Length: [0-9]+" | grep -oE "[0-9]+")
-t=$(awk 'BEGIN{printf int('$MBlimit'%'$Length'/'$length')}')
-T=$(awk 'BEGIN{printf int('$MBlimit'/'$Length')}')
-FMB=$(awk 'BEGIN{printf "%.2f\n",(('$MBlimit'-('$MBlimit'%'$Length'%'$length'))/1024/1024)}')
-FGB=$(awk 'BEGIN{printf "%.3f\n",(('$MBlimit'-('$MBlimit'%'$Length'%'$length'))/1024/1024/1024)}')
+main(){
+	length=$(wget --spider -U "$UA" $url -SO- /dev/null 2>&1 | grep -oE "Content-Length: [0-9]+" | grep -oE "[0-9]+")
+	Length=$(wget --spider -U "$UA" $Url -SO- /dev/null 2>&1 | grep -oE "Content-Length: [0-9]+" | grep -oE "[0-9]+")
+	t=$(awk 'BEGIN{printf int('$MBlimit'%'$Length'/'$length')}')
+	T=$(awk 'BEGIN{printf int('$MBlimit'/'$Length')}')
+	FMB=$(awk 'BEGIN{printf "%.2f\n",(('$MBlimit'-('$MBlimit'%'$Length'%'$length'))/1024/1024)}')
+	FGB=$(awk 'BEGIN{printf "%.3f\n",(('$MBlimit'-('$MBlimit'%'$Length'%'$length'))/1024/1024/1024)}')
 
-echo $(date) Mission $(awk 'BEGIN{printf "%.f\n",('$MBlimit'/1024/1024)}') MB. Starting...
+	echo $(date) Mission $(awk 'BEGIN{printf "%.f\n",('$MBlimit'/1024/1024)}') MB. Starting...
 
-if [ "$t" -gt 0 ]; then
-	echo $(date) $(echo $url | awk -F"/" '{print $NF}') - $(awk 'BEGIN{printf "%.1f\n",('$length'/1024/1024)}') MB will be downloaded $t times...
-	for((i = 1; i <= t; i++))
-	do
-		s=$(wget -U "$UA" -SO- $url 2>&1 >/dev/null | grep -E "written to stdout" | awk -F"[written]" '{print $1}')
-		echo $s $i - $(awk 'BEGIN{printf "%.1f\n",('$i'*'$length'/1024/1024)}') MB
-	done
-fi
+		if [ "$t" -gt 0 ]; then
+			echo $(date) $(echo $url | awk -F"/" '{print $NF}') - $(awk 'BEGIN{printf "%.1f\n",('$length'/1024/1024)}') MB will be downloaded $t times...
+			for((i = 1; i <= t; i++))
+			do
+				s=$(wget -U "$UA" -SO- $url 2>&1 >/dev/null | grep -E "written to stdout" | awk -F"[written]" '{print $1}')
+				echo $s $i - $(awk 'BEGIN{printf "%.1f\n",('$i'*'$length'/1024/1024)}') MB
+			done
+		fi
 
-if [ "$T" -gt 0 ]; then
-	echo $(date) $(echo $Url | awk -F"/" '{print $NF}') - $(awk 'BEGIN{printf "%.1f\n",('$Length'/1024/1024)}') MB will be downloaded $T times...
-	for((j = 1; j <= T; j++))
-	do
-		S=$(wget -U "$UA" -SO- $Url 2>&1 >/dev/null | grep -E "written to stdout" | awk -F"[written]" '{print $1}')
-		echo $S $j - $(awk 'BEGIN{printf "%.1f\n",('$j'*'$Length'/1024/1024)}') MB
-	done
-fi
+		if [ "$T" -gt 0 ]; then
+			echo $(date) $(echo $Url | awk -F"/" '{print $NF}') - $(awk 'BEGIN{printf "%.1f\n",('$Length'/1024/1024)}') MB will be downloaded $T times...
+			for((j = 1; j <= T; j++))
+			do
+				S=$(wget -U "$UA" -SO- $Url 2>&1 >/dev/null | grep -E "written to stdout" | awk -F"[written]" '{print $1}')
+				echo $S $j - $(awk 'BEGIN{printf "%.1f\n",('$j'*'$Length'/1024/1024)}') MB
+			done
+		fi
 
-echo $(date) Mission $(awk 'BEGIN{printf "%.f\n",('$MBlimit'/1024/1024)}') MB. Accomplished $FMB MB \($FGB GB\). Thanks!
+		echo $(date) Mission $(awk 'BEGIN{printf "%.f\n",('$MBlimit'/1024/1024)}') MB. Accomplished $FMB MB \($FGB GB\). Thanks!
+}
+
+main
