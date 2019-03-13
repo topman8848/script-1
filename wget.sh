51f5a874a590d5354a970232db4cb44b2f09ab6d
Mcl <mixool0204@gmail.com>
2019-03-13 23:12:35 +0800

    Update wget.sh

--- wget.sh
+++ wget.sh
@@ -48,4 +48,4 @@ fi
 timer_end=`date "+%Y-%m-%d %H:%M:%S"`
 duration=`echo $(($(date +%s -d "${timer_end}") - $(date +%s -d "${timer_start}"))) | awk '{t=split("60 s 60 m 24 h 999 d",a);for(n=1;n<t;n+=2){if($1==0)break;s=$1%a[n]a[n+1]s;$1=int($1/a[n])}print s}'`
 
-echo $(date) Mission $(awk 'BEGIN{printf "%.f\n",('$MBlimit'/1024/1024)}') MB. Accomplished $FMB MB \($FGB GB\) in $duration. Thanks! 
+echo $(date) Mission $(awk 'BEGIN{printf "%.f\n",('$MBlimit'/1024/1024)}') MB. Accomplished $FMB MB \($FGB GB\) $duration. Thanks!
