e3d4bb372bdad59bbcbeb17b2c1ae55c62a0d4b8
Mcl <mixool0204@gmail.com>
2019-03-30 23:21:05 +0800

    Update gost.sh

--- gost.sh
+++ gost.sh
@@ -8,12 +8,8 @@ VER="$(wget -qO- https://github.com/ginuerzh/gost/tags | grep -oE "/ginuerzh/gos
 VER=${VER:=2.7.2}
 URL="https://github.com/ginuerzh/gost/releases/download/v${VER}/gost_${VER}_linux_amd64.tar.gz"
 
-echo -e "Clean up gost"
-systemctl disable gost.service
-killall -9 gost
+echo "Downloading gost_${VER} to /root/gost from $URL"
 rm -rf /root/gost
-
-echo "Downloading gost_${VER} from $URL"
 wget -O /root/gost_${VER}_linux_amd64.tar.gz $URL
 tar -zxvf /root/gost_${VER}_linux_amd64.tar.gz && cd /root/gost_${VER}_linux_amd64 && mv gost /root/gost && cd
 rm -rf /root/gost_${VER}_linux_amd64*
@@ -31,14 +27,4 @@ User=root
 WantedBy=multi-user.target
 EOF
 
-echo "Starting gost service"
-systemctl enable gost.service && systemctl daemon-reload && systemctl start gost.service
-
-if systemctl status gost >/dev/null; then
-	echo "gost started."
-	echo -e "vi /etc/systemd/system/gost.service as needed."
-	echo -e "systemctl daemon-reload && systemctl restart gost.service for restart."
-else
-	echo "gost start failed."
-	systemctl status gost -l	
-fi
+systemctl enable gost.service && systemctl daemon-reload && systemctl restart gost.service && systemctl status gost -l
