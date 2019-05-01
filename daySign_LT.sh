3673bac25c5f8cb1107ddeedfa4089ede4eb9c6d
Mcl <mixool0204@gmail.com>
2019-05-02 01:04:39 +0800

    nice

--- daySign_LT.sh
+++ daySign_LT.sh
@@ -4,15 +4,16 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 ## wget --no-check-certificate https://raw.githubusercontent.com/mixool/script/debian-9/daySign_LT.sh && chmod +x daySign_LT.sh && bash daySign_LT.sh
 ### bash <(curl -s https://raw.githubusercontent.com/mixool/script/debian-9/daySign_LT.sh) ${username} ${password}
 
-# info_user: change them to yours or use parameters instead.
+# user info: change them to yours or use parameters instead.
 username="$1"
 password="$2"
 
 # deviceId: if you failed to login , maybe you need to change it to your IMEI.
-deviceId="213313966308801"
+deviceId=$(shuf -i 123456789012345-987654321012345 -n 1)
 
 # urls
 login_url="http://m.client.10010.com/mobileService/login.htm"
+logout_url="https://m.client.10010.com/mobileService/logout.htm"
 query_url="https://act.10010.com/SigninApp/signin/querySigninActivity.htm"
 sign_url="https://act.10010.com/SigninApp/signin/daySign.do"
 gold_url="https://act.10010.com/SigninApp/signin/goldTotal.do"
@@ -47,18 +48,14 @@ function urlencode() {
 }
 
 function daySign() {
-  urlencode_username=$(urlencode $crypt_username)
-  urlencode_password=$(urlencode $crypt_password)
-  timestamp=$(date +"%Y-%m-%d_%H-%M-%S" | awk -F'[-_-]' '{print $1$2$3$4$5$6}')
-
     cat > $workdir/signdata <<-EOF
 isRemberPwd=true
 &deviceId=$deviceId
-&password=$urlencode_password
+&password=$(urlencode $crypt_password)
 &netWay=Wifi
-&mobile=$urlencode_username
+&mobile=$(urlencode $crypt_username)
 &yw_code: 
-&timestamp=$timestamp
+&timestamp=$(date +"%Y-%m-%d_%H-%M-%S" | awk -F'[-_-]' '{print $1$2$3$4$5$6}')
 &appId=dda726c5e6aa1ee96e62a88ecae46f11635696d85fc21cff4333b0eded85fc21dd4177d8ee50e52b977ee1d25e032b961585631b4fc010c2f1ac8c8e04a6791e
 &keyVersion:
 &deviceBrand=Oneplus
@@ -70,63 +67,75 @@ isRemberPwd=true
 &deviceCode=$deviceId
 EOF
 
-  # querySigninActivity cookies
-  curl -s -D $workdir/cookie_A.txt -d @$workdir/signdata $login_url >/dev/null
-  token=$(cat $workdir/cookie_A.txt | grep -oE "a_token=.*" | awk -F"a_token=" '{print $2}')
-  [[ "$token" = "" ]] && echo "Error, starting daySign failed." && exit 1
+  # on_token cookies
+  curl -s -D $workdir/on_token $logout_url >/dev/null
+  
+  # cookie_a cookie_b
+  curl -s -b $workdir/on_token -c $workdir/cookie_a -d @$workdir/signdata $login_url >/dev/null
+  token=$(cat $workdir/cookie_a | grep -E "a_token" | awk  '{print $7}')
+  [[ "$token" = "" ]] && echo "Error, starting failed." && rm -rf $workdir && exit 1
   echo 
   echo $(date) starting daySign...
-  curl -s -b $workdir/cookie_A.txt -c $workdir/cookie_B.txt --data "token=$token" $query_url >/dev/null
+  curl -s -b $workdir/cookie_a -c $workdir/cookie_b --data "token=$token" $query_url >/dev/null
 
   # goldTotal_before
-  echo goldTotal_before：$(curl -s -b $workdir/cookie_B.txt $gold_url)
+  echo goldTotal_before：$(curl -s -b $workdir/cookie_b $gold_url)
 
   # daySign_status
-  echo daySign_status： $(curl -s -b $workdir/cookie_B.txt $sign_url)
+  echo daySign_status： $(curl -s -b $workdir/cookie_b $sign_url)
   
   # weiboSign_status
   timestamp=$(date +"%Y-%m-%d_%H-%M-%S" | awk -F'[-_-]' '{print $1$2$3$4$5$6}')
-  random_weibo_stamp=$(shuf -i 1234567890123456-9876543210123456 -n 1)
-  echo weiboSign_status：$(curl -s -b $workdir/cookie_B.txt --data "transId=$timestamp.$random_weibo_stamp&userNumber=$username&taskCode=TA590934984&finishTime=$timestamp&taskType=DAILY_TASK" https://act.10010.com/signinAppH/commonTask)
+  random_weibo_stamp=$(shuf -i 123456789012345-987654321012345 -n 1)
+  echo weiboSign_status：$(curl -s -b $workdir/cookie_b --data "transId=$timestamp$(shuf -i 0-9 -n 1).$random_weibo_stamp&userNumber=$username&taskCode=TA590934984&finishTime=$timestamp&taskType=DAILY_TASK" https://act.10010.com/signinAppH/commonTask)
 
   # goldTotal_now
-  echo goldTotal_now：$(curl -s -b $workdir/cookie_B.txt $gold_url)
+  echo goldTotal_now：$(curl -s -b $workdir/cookie_b $gold_url)
 }
 
 function doubleball() {
-  # doubleball: 3 times free each day.
-  usernumberofjsp=$(curl -s -b $workdir/cookie_B.txt -c $workdir/cookie_C.txt http://m.client.10010.com/dailylottery/static/textdl/userLogin | grep -oE "encryptmobile=\w*" | awk -F"encryptmobile=" '{print $2}')
-  [[ "$usernumberofjsp" = "" ]] && echo "Error, starting doubleball failed." && return 1
+  # doubleball: 3 times free each day. need cookie_b
+  usernumberofjsp=$(curl -s -b $workdir/cookie_b http://m.client.10010.com/dailylottery/static/textdl/userLogin | grep -oE "encryptmobile=\w*" | awk -F"encryptmobile=" '{print $2}')
   echo 
   echo $(date) starting doubleball...
-  echo 1st： $(curl -s -b $workdir/cookie_C.txt --data "usernumberofjsp=$usernumberofjsp" http://m.client.10010.com/dailylottery/static/doubleball/choujiang) ; sleep 3
-  echo 2nd： $(curl -s -b $workdir/cookie_C.txt --data "usernumberofjsp=$usernumberofjsp" http://m.client.10010.com/dailylottery/static/doubleball/choujiang) ; sleep 3
-  echo 3rd： $(curl -s -b $workdir/cookie_C.txt --data "usernumberofjsp=$usernumberofjsp" http://m.client.10010.com/dailylottery/static/doubleball/choujiang) ; sleep 3
+  echo 1st： $(curl -s -b $workdir/cookie_b --data "usernumberofjsp=$usernumberofjsp" http://m.client.10010.com/dailylottery/static/doubleball/choujiang) ; sleep 3
+  echo 2nd： $(curl -s -b $workdir/cookie_b --data "usernumberofjsp=$usernumberofjsp" http://m.client.10010.com/dailylottery/static/doubleball/choujiang) ; sleep 3
+  echo 3rd： $(curl -s -b $workdir/cookie_b --data "usernumberofjsp=$usernumberofjsp" http://m.client.10010.com/dailylottery/static/doubleball/choujiang) ; sleep 3
 }
 
 
-function tree() {
+function arborday() {
+  # arborday: 1 time free each day. need cookie_a
   echo 
   echo $(date) starting arbor day...
   timestamp=$(date +"%Y-%m-%d_%H-%M-%S" | awk -F'[-_-]' '{print $1$2$3$4$5$6}')
-  curl -s -b $workdir/cookie_B.txt -c $workdir/cookie_D.txt --data "timestamp=$timestamp&desmobile=$username&version=android%406.0100" https://m.client.10010.com/mactivity/arborday/index >/dev/null ; sleep 3
-  curl -s -b $workdir/cookie_D.txt -c $workdir/cookie_E.txt --data "transId=$timestamp$(shuf -i 123-987 -n 1)&phoneNum=$username&serviceCode=takeActivityInfo&channel=mobileClient&url=https%3A%2F%2Fm.client.10010.com%2Fmactivity%2Farborday%2Findex&city=831" https://m.client.10010.com/freegift-interface/appUrlShare/takeActivityInfo ; sleep 3
-  curl -s -b $workdir/cookie_E.txt -c $workdir/cookie_H.txt --data "thirdUrl=https%3A%2F%2Fm.client.10010.com%2Fmactivity%2Farborday%2Findex" https://m.client.10010.com/mobileService/customer/getShareRedisInfo.htm  ; sleep 3 
-  curl -s -b $workdir/cookie_H.txt https://m.client.10010.com/mactivity/arborday/arbor/1/0/1/grow
+  curl -s -b $workdir/cookie_a -c $workdir/cookie_ar.txt --data "timestamp=$timestamp&desmobile=$username&version=android%406.0100" https://m.client.10010.com/mactivity/arborday/index >/dev/null ; sleep 3
+  curl -s -b $workdir/cookie_ar.txt https://m.client.10010.com/mactivity/arborday/arbor/1/0/1/grow
+}
+
+function wangzuan() {
+  # wangzuan: 1 time free each month. need cookie_a
+  [[ $(date | awk '{print $3}') -eq 1 ]] || return 0
+  echo 
+  echo $(date) starting wangzuan...
+  timestamp=$(date +"%Y-%m-%d_%H-%M-%S" | awk -F'[-_-]' '{print $1$2$3$4$5$6}')
+  curl -L -s -b $workdir/cookie_a -c $workdir/cookie_wa.txt --data "timestamp=$timestamp&desmobile=$username&version=android%406.0100" "https://m.client.10010.com/mobileService/openPlatform/openPlatLine.htm?to_url=https://wangzuan.10010.com/api/auth/login?source=2" >/dev/null ; sleep 3 
+  echo wangzuan_status：$(curl -X POST -s -b $workdir/cookie_wa.txt https://wangzuan.10010.com/api/activity/lottery)
 }
 
 function main() {
   rsaencrypt
   daySign
   doubleball
-  tree
+  arborday
+  wangzuan
   
   # clean
   rm -rf $workdir
 
   # exit
   echo 
-  echo $(date) Accomplished. Thanks! && exit 0
+  echo $(date) Accomplished. Thanks!
 }
 
 main
