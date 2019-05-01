36214df2fee9aa8c8fbb62f2ef045dfa48fc25a8
Mcl <mixool0204@gmail.com>
2019-05-01 12:05:32 +0800

    arbor day

--- daySign_LT.sh
+++ daySign_LT.sh
@@ -17,8 +17,12 @@ query_url="https://act.10010.com/SigninApp/signin/querySigninActivity.htm"
 sign_url="https://act.10010.com/SigninApp/signin/daySign.do"
 gold_url="https://act.10010.com/SigninApp/signin/goldTotal.do"
 
+# workdir
+workdir="/root/daySign_LT"
+[[ ! -d "$workdir" ]] && mkdir $workdir
+
 function rsaencrypt() {
-  cat > /tmp/rsa_public.key <<-EOF
+  cat > $workdir/rsa_public.key <<-EOF
 -----BEGIN PUBLIC KEY-----
 MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDc+CZK9bBA9IU+gZUOc6
 FUGu7yO9WpTNB0PzmgFBh96Mg1WrovD1oqZ+eIF4LjvxKXGOdI79JRdve9
@@ -27,8 +31,8 @@ aqGfLgKvZHOnwTjyNqjBUxzMeQlEC2czEMSwIDAQAB
 -----END PUBLIC KEY-----
 EOF
 
-  crypt_username=$(echo -n $username | openssl rsautl -encrypt -inkey /tmp/rsa_public.key -pubin -out >(base64 | tr "\n" " " | sed s/[[:space:]]//g))
-  crypt_password=$(echo -n $password | openssl rsautl -encrypt -inkey /tmp/rsa_public.key -pubin -out >(base64 | tr "\n" " " | sed s/[[:space:]]//g))
+  crypt_username=$(echo -n $username | openssl rsautl -encrypt -inkey $workdir/rsa_public.key -pubin -out >(base64 | tr "\n" " " | sed s/[[:space:]]//g))
+  crypt_password=$(echo -n $password | openssl rsautl -encrypt -inkey $workdir/rsa_public.key -pubin -out >(base64 | tr "\n" " " | sed s/[[:space:]]//g))
 }
 
 function urlencode() {
@@ -48,7 +52,7 @@ function daySign() {
   timestamp=$(date +"%Y-%m-%d_%H-%M-%S" | awk -F'[-_-]' '{print $1$2$3$4$5$6}')
   random_weibo_stamp=$(shuf -i 1234567890123456-9876543210123456 -n 1)
 
-    cat > /tmp/signdata <<-EOF
+    cat > $workdir/signdata <<-EOF
 isRemberPwd=true
 &deviceId=$deviceId
 &password=$urlencode_password
@@ -68,44 +72,59 @@ isRemberPwd=true
 EOF
 
   # querySigninActivity cookies
-  curl -s -D ./cookie_D.txt -d @/tmp/signdata $login_url >/dev/null
-  token=$(cat ./cookie_D.txt | grep -oE "a_token=.*" | awk -F"a_token=" '{print $2}')
+  curl -s -D $workdir/cookie_A.txt -d @$workdir/signdata $login_url >/dev/null
+  token=$(cat $workdir/cookie_A.txt | grep -oE "a_token=.*" | awk -F"a_token=" '{print $2}')
   [[ "$token" = "" ]] && echo "Error, starting daySign failed." && exit 1
   echo 
   echo $(date) starting daySign...
-  curl -s -b ./cookie_D.txt -c ./cookie_E.txt --data "token=$token" $query_url >/dev/null
+  curl -s -b $workdir/cookie_A.txt -c $workdir/cookie_B.txt --data "token=$token" $query_url >/dev/null
 
   # goldTotal_before
-  echo goldTotal_before：$(curl -s -b ./cookie_E.txt $gold_url)
+  echo goldTotal_before：$(curl -s -b $workdir/cookie_B.txt $gold_url)
 
   # daySign_status
-  echo daySign_status： $(curl -s -b ./cookie_E.txt $sign_url)
+  echo daySign_status： $(curl -s -b $workdir/cookie_B.txt $sign_url)
   
   # weiboSign_status
-  echo weiboSign_status：$(curl -s -b ./cookie_E.txt --data "transId=$timestamp$random_weibo_stamp&userNumber=$username&taskCode=TA590934984&finishTime=$timestamp&taskType=DAILY_TASK" https://act.10010.com/signinAppH/commonTask)
+  echo weiboSign_status：$(curl -s -b $workdir/cookie_B.txt --data "transId=$timestamp.$random_weibo_stamp&userNumber=$username&taskCode=TA590934984&finishTime=$timestamp&taskType=DAILY_TASK" https://act.10010.com/signinAppH/commonTask)
 
   # goldTotal_now
-  echo goldTotal_now：$(curl -s -b ./cookie_E.txt $gold_url)
+  echo goldTotal_now：$(curl -s -b $workdir/cookie_B.txt $gold_url)
 }
 
 function doubleball() {
   # doubleball: 3 times free each day.
-  usernumberofjsp=$(curl -s -b ./cookie_E.txt  -c ./cookie_F.txt http://m.client.10010.com/dailylottery/static/textdl/userLogin | grep -oE "encryptmobile=\w*" | awk -F"encryptmobile=" '{print $2}')
+  usernumberofjsp=$(curl -s -b $workdir/cookie_B.txt -c $workdir/cookie_C.txt http://m.client.10010.com/dailylottery/static/textdl/userLogin | grep -oE "encryptmobile=\w*" | awk -F"encryptmobile=" '{print $2}')
   [[ "$usernumberofjsp" = "" ]] && echo "Error, starting doubleball failed." && exit 1
   echo 
   echo $(date) starting doubleball...
-  echo 1st： $(curl -s -b ./cookie_F.txt --data "usernumberofjsp=$usernumberofjsp" http://m.client.10010.com/dailylottery/static/doubleball/choujiang) ; sleep 3
-  echo 2nd： $(curl -s -b ./cookie_F.txt --data "usernumberofjsp=$usernumberofjsp" http://m.client.10010.com/dailylottery/static/doubleball/choujiang) ; sleep 3
-  echo 3rd： $(curl -s -b ./cookie_F.txt --data "usernumberofjsp=$usernumberofjsp" http://m.client.10010.com/dailylottery/static/doubleball/choujiang) ; sleep 3
+  echo 1st： $(curl -s -b $workdir/cookie_C.txt --data "usernumberofjsp=$usernumberofjsp" http://m.client.10010.com/dailylottery/static/doubleball/choujiang) ; sleep 3
+  echo 2nd： $(curl -s -b $workdir/cookie_C.txt --data "usernumberofjsp=$usernumberofjsp" http://m.client.10010.com/dailylottery/static/doubleball/choujiang) ; sleep 3
+  echo 3rd： $(curl -s -b $workdir/cookie_C.txt --data "usernumberofjsp=$usernumberofjsp" http://m.client.10010.com/dailylottery/static/doubleball/choujiang) ; sleep 3
 }
 
-rsaencrypt
-daySign
-doubleball
 
-# clean
-rm /tmp/rsa_public.key　/tmp/signdata　./cookie_D.txt ./cookie_E.txt ./cookie_F.txt
+function tree() {
+  echo 
+  echo $(date) starting arbor day...
+  curl -s -b $workdir/cookie_B.txt -c $workdir/cookie_D.txt --data "timestamp=$timestamp&desmobile=$username&version=android%406.0100" https://m.client.10010.com/mactivity/arborday/index >/dev/null ; sleep 3
+  curl -s -b $workdir/cookie_D.txt -c $workdir/cookie_E.txt --data "transId=$timestamp$(shuf -i 100-600 -n 1)&phoneNum=$username&serviceCode=takeActivityInfo&channel=mobileClient&url=https%3A%2F%2Fm.client.10010.com%2Fmactivity%2Farborday%2Findex&city=831" https://m.client.10010.com/freegift-interface/appUrlShare/takeActivityInfo ; sleep 3
+  curl -s -b $workdir/cookie_E.txt -c $workdir/cookie_H.txt --data "thirdUrl=https%3A%2F%2Fm.client.10010.com%2Fmactivity%2Farborday%2Findex" https://m.client.10010.com/mobileService/customer/getShareRedisInfo.htm  ; sleep 3 
+  curl -s -b $workdir/cookie_H.txt https://m.client.10010.com/mactivity/arborday/arbor/1/0/1/grow
+}
 
-# exit
-echo 
-echo $(date) Accomplished. Thanks! && exit 0
+function main() {
+  rsaencrypt
+  daySign
+  doubleball
+  tree
+  
+  # clean
+  rm -rf $workdir
+
+  # exit
+  echo 
+  echo $(date) Accomplished. Thanks! && exit 0
+}
+
+main
