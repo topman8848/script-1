850c1137220ce34c512d3613599033be10f0bcdd
Mcl <mixool0204@gmail.com>
2019-05-03 14:52:01 +0800

    add function member&&openChg

--- daySign_LT.sh
+++ daySign_LT.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
 # Usage:
 ## wget --no-check-certificate https://raw.githubusercontent.com/mixool/script/debian-9/daySign_LT.sh && chmod +x daySign_LT.sh && bash daySign_LT.sh
 ### bash <(curl -s https://raw.githubusercontent.com/mixool/script/debian-9/daySign_LT.sh) ${username} ${password}
