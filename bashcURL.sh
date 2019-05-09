fd7036816cd727f6a8774a78092e751c9e33483f
Mcl <mixool0204@gmail.com>
2019-05-09 12:20:06 +0800

    Update bashcURL.sh

--- bashcURL.sh
+++ bashcURL.sh
@@ -2,26 +2,29 @@
 # Usage:
 #   curl https://raw.githubusercontent.com/mixool/script/master/bashcURL.sh | bash
 ##  wget --no-check-certificate https://raw.githubusercontent.com/mixool/script/master/bashcURL.sh && chmod +x bashcURL.sh && ./bashcURL.sh
-MBlimit=1024
+
+#
+limit_MB=1000
 bashcURL=""
 cmd1="$bashcURL -I -s"
 cmd2="$bashcURL -o /dev/null"
-Length=$(eval $cmd1 | grep -oE "Content-Length: [0-9]+" | grep -oE "[0-9]+")
 
-for((i = 1; i >= 1; i++))
-do
-	echo Downloading $i...
-	eval $cmd2
-	Total=$(awk 'BEGIN{printf ('$i'*'$Length')}')
-	Ltotal=$(awk 'BEGIN{printf "%.f\n",('$MBlimit'*1024*1024-'$Length')}')
-	MBtotal=$(awk 'BEGIN{printf "%.4f\n",('$i'*'$Length'/1024/1024)}')
-	GBtotal=$(awk 'BEGIN{printf "%.4f\n",('$i'*'$Length'/1024/1024/1024)}')
-	
-	if [ "$Ltotal" -lt "$Total" ]
-	then
-	echo $MBtotal MB had been downloaded, That is about $GBtotal GB, Thanks!
-	break
-	fi
-	echo $MBtotal MB had been downloaded, That is about $GBtotal GB.
-	
-done
+#
+limit=$(awk 'BEGIN{printf "%.f\n",('$limit_MB'*1024*1024)}')
+Length=$(eval $cmd1 | grep -oE "Content-Length: [0-9]+" | grep -oE "[0-9]+")
+Length_MB=$(awk 'BEGIN{printf "%.1f\n",('$Length'/1024/1024)}')
+T=$(awk 'BEGIN{printf int('$limit'/'$Length')}')
+FMB=$(awk 'BEGIN{printf "%.2f\n",(('$limit'-('$limit'%'$Length'))/1024/1024)}')
+FGB=$(awk 'BEGIN{printf "%.3f\n",(('$limit'-('$limit'%'$Length'))/1024/1024/1024)}')
+
+if [ "$T" -gt 0 ]; then
+	echo $(date) Mission $limit_MB MB. $Length_MB MB will be downloaded $T times...
+	for((i = 1; i <= T; i++))
+	do
+		echo; echo $(awk 'BEGIN{printf "%.1f\n",('$i'*'$Length'/1024/1024)}') MB $i...
+		eval $cmd2 -#
+	done
+	echo
+fi
+
+echo $(date) Mission $limit_MB MB. Accomplished $FMB MB \($FGB GB\). Thanks!
