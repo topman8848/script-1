8a141fbc2b7dd6a80477f6c791fd1b8e237f360a
Mcl <mixool0204@gmail.com>
2019-09-29 10:48:03 +0800

    Update warp_plus.sh

--- warp_plus.sh
+++ warp_plus.sh
@@ -1,24 +1,22 @@
 #!/usr/bin/env bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
-# Usage: warp.plus referrer  !!!!replace "23320584-5625-4f47-89a8-c760a29b0a1e " to yours!!!!
+# Usage: warp.plus referrer  !!!!replace "6584a16f-eb84-4751-b137-8675e3fefc9e" to yours!!!!
 ## apt update && apt instal parallel -y
-#### bash ./warp_plus.sh 10 100
+#### bash ./warp_plus.sh 100 20
 
-# thread && loopss && defaut is 20 && 5
-export thread="$1"
-export loopss="$2"
+# mission with threads, defaut: {20} {10}
+mission="$1"
+threads="$2"
 
-echo; echo $(date) Mission $((${thread:=20}*${loopss:=5})) GB. Starting with ${thread} threads and ${loopss} loops...
+echo; echo $(date) Mission ${mission:=20} GB. Starting with ${threads:=10} threads...
 
 curldetails() {
-	for thread in $(seq ${loopss}); do
-		curl -X POST -sA "okhttp/3.12.1" -H 'content-type: application/json' --data "{\"key\": \"$(tr -dc '=_+/A-Za-z0-9' </dev/urandom | head -c 43)=\",\"referrer\": \"23320584-5625-4f47-89a8-c760a29b0a1e\",\"warp_enabled\": false,\"tos\": \"$(date -u +%FT%T.$(shuf -i 123-987 -n 1)Z)\",\"type\": \"Android\",\"locale\": \"en_US\"}"  "https://api.cloudflareclient.com/v0a745/reg" >/dev/null
-	done
+	curl -X POST -sA "okhttp/3.12.1" -H 'content-type: application/json' --data "{\"key\": \"$(tr -dc '=_+/A-Za-z0-9' </dev/urandom | head -c 43)=\",\"referrer\": \"$1\",\"warp_enabled\": false,\"tos\": \"$(date -u +%FT%T.$(shuf -i 123-987 -n 1)Z)\",\"type\": \"Android\",\"locale\": \"en_US\"}"  "https://api.cloudflareclient.com/v0a745/reg" >/dev/null
 }
 
 export -f curldetails
 
-seq ${thread} | parallel -j0 curldetails
+seq ${mission} | parallel -j${threads} --bar curldetails "6584a16f-eb84-4751-b137-8675e3fefc9e"
 
 echo $(date) Mission Accomplished Thanks!
