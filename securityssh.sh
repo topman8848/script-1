6274901fe036dfe763b38a8f38e59034466be454
Mcl <mixool0204@gmail.com>
2019-12-04 10:43:36 +0800

    Update securityssh.sh

--- securityssh.sh
+++ securityssh.sh
@@ -2,23 +2,35 @@
 # Usage:
 #   curl https://raw.githubusercontent.com/mixool/script/debian-9/securityssh.sh | bash
 
-if [ "$(id -u)" != "0" ]; then
-    echo "ERROR: Please run as root"
-    exit 1
+[[ "$(id -u)" != "0" ]] && echo "ERROR: Please run as root" && exit 1
+
+# custom port
+echo "Securing your SSH server with custom port..."
+SSH_PORT=${SSH_PORT:-n}
+while ! [[ ${SSH_PORT} =~ ^[0-9]+$ ]]; do
+    read -rp "Custom SSH port (default SSH port is 22): " -e SSH_PORT
+done
+
+if [[ ${SSH_PORT} =~ ^[0-9]+$ ]]; then
+    if grep -qwE "^Port\ [0-9]*" /etc/ssh/sshd_config; then
+        sed -i "s/^Port\ [0-9]*/Port\ ${SSH_PORT}/g" /etc/ssh/sshd_config
+    else
+        sed -i "/^#Port\ [0-9]*/a Port\ ${SSH_PORT}" /etc/ssh/sshd_config
+    fi
+	echo "SSH port updated to ${SSH_PORT}."
+else
+    echo "Unable to update SSH port."
 fi
 
-# Informations
-read -p "Please input ssh port:" port </dev/tty
-read -p "Please input the public key:" key </dev/tty
+# custom rsa_pub_key login
+RSA_PUB_KEY=${RSA_PUB_KEY:-n}
+while ! [[ ${RSA_PUB_KEY} =~ ssh-rsa* ]]; do
+    read -rp ": " -e RSA_PUB_KEY
+done
 
-# Add key
-mkdir -p ~/.ssh && chmod 700 ~/.ssh
+[[ ! -d "~/.ssh" ]] && mkdir -p "~/.ssh" && chmod 700 ~/.ssh
 echo $key >> ~/.ssh/authorized_keys
-
-# Config 
-cp /etc/ssh/sshd_config /etc/ssh/sshd_config_bak
-echo -e "Port ${port}" >> /etc/ssh/sshd_config
 sed -i "s/PermitRootLogin.*/PermitRootLogin without-password/g" /etc/ssh/sshd_config
 
 # Active
-systemctl reload sshd.service
+service ssh restart
