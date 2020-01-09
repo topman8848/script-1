08826431f5f1b73a6d56074497c96727276870d2
Mcl <mixool0204@gmail.com>
2020-01-09 13:41:37 +0800

    Update gost.sh

--- gost.sh
+++ gost.sh
@@ -5,11 +5,12 @@
 METHOD="-L=mwss://:443 -L=http2://:444"
 
 VER="$(wget -qO- https://github.com/ginuerzh/gost/tags | grep -oE "/tag/v.*" | sed -n '1p' | sed 's/\".*//;s/^.*v//')"
-VER=${VER:=2.7.2}
+VER=${VER:=2.8.2}
 URL="https://github.com/ginuerzh/gost/releases/download/v${VER}/gost-linux-amd64-${VER}.gz"
 
 echo "1. Downloading gost-linux-amd64-${VER}.gz to /root/gost from $URL" && echo
-wget -O - $URL | gzip -d > /root/gost && chmod +x /root/gost
+[[ -f "/root/gost" ]] && rm -rf /root/gost
+wget -O - $URL | gzip -d > /root/gost && chmod +x /root/gost || (echo Woooopss,err!!! && exit 1)
 
 echo "2. Generate /etc/systemd/system/gost.service"
 cat <<EOF > /etc/systemd/system/gost.service
