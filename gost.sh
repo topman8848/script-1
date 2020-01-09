22e9abe365f634ea7ffabd4b30741520efc5b24b
Mcl <mixool0204@gmail.com>
2020-01-09 13:42:03 +0800

    Update gost.sh

--- gost.sh
+++ gost.sh
@@ -6,7 +6,7 @@ METHOD="-L=mwss://:443 -L=http2://:444"
 
 VER="$(wget -qO- https://github.com/ginuerzh/gost/tags | grep -oE "/tag/v.*" | sed -n '1p' | sed 's/\".*//;s/^.*v//')"
 VER=${VER:=2.8.2}
-URL="https://github.com/ginuerzh/gost/releases/download/v${VER}/gost-linux-amd64-${VER}.gz"
+URL="httttps://github.com/ginuerzh/gost/releases/download/v${VER}/gost-linux-amd64-${VER}.gz"
 
 echo "1. Downloading gost-linux-amd64-${VER}.gz to /root/gost from $URL" && echo
 [[ -f "/root/gost" ]] && rm -rf /root/gost
